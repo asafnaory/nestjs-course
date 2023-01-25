7df6e23dd4042396640a3973268c2b3a2e5a53c5
Asaf Naory <asaf.naory1@gmail.com>
2023-01-25 20:55:03 +0200

    fix handleErrors

--- lesson-5-authentication-1/src/auth/auth.service.ts
+++ lesson-5-authentication-1/src/auth/auth.service.ts
@@ -7,57 +7,65 @@ import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcryptjs';
+import { handleErrors } from 'src/helpers/helpers';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
   async signup(dto: AuthCredentialsDto): Promise<User> {
-    const { email, password } = dto;
+    try {
+      const { email, password } = dto;
 
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) {
-      // user already exists
-      throw new ConflictException('User with that email already exists');
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (user) {
+        // user already exists
+        throw new ConflictException('User with that email already exists');
+      }
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      console.log({ salt, hashedPassword });
+
+      const newAgent = await this.prisma.user.create({
+        data: {
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      return newAgent;
+    } catch (e: unknown) {
+      handleErrors(e);
     }
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    console.log({ salt, hashedPassword });
-
-    const newAgent = await this.prisma.user.create({
-      data: {
-        email,
-        password: hashedPassword,
-      },
-    });
-
-    return newAgent;
-    
   }
   async signin(dto: AuthCredentialsDto): Promise<string> {
-    const { email, password } = dto;
-    const user = await this.prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
+    try {
+      const { email, password } = dto;
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
 
-    //if no user found...
-    if (!user) {
-      throw new UnauthorizedException('Wrong credentials...');
+      //if no user found...
+      if (!user) {
+        throw new UnauthorizedException('Wrong credentials...');
+      }
+
+      // check if the password is valid
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (!validPassword) {
+        throw new UnauthorizedException('Wrong credentials...');
+      }
+
+      return 'OK';
+    } catch (e: unknown) {
+      handleErrors(e);
     }
-
-    // check if the password is valid
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword) {
-      throw new UnauthorizedException('Wrong credentials...');
-    }
-
-    return 'OK';
   }
 }
